c465b6cca53d68c075b11bc749092db5c4e99ef9
birkin <birkin.diana@gmail.com>
2021-02-01 10:05:59 -0500

    updates url syntax.

--- config/urls.py
+++ config/urls.py
@@ -1,38 +1,47 @@
 # -*- coding: utf-8 -*-
 
-# import usep_app
-from django.conf.urls import patterns, include, url
+from __future__ import unicode_literals
+
+# from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import RedirectView
+from usep_app import search, views
 
 
 admin.autodiscover()
 
 
-urlpatterns = patterns('',
+urlpatterns = [
 
-    url( r'^usep/admin/links/$',  'usep_app.views.admin_links', name='admn_links_url' ),
-    url( r'^usep/admin/', include(admin.site.urls) ),
+    ## primary app urls...
 
-    url( r'^usep/info/$',  'usep_app.views.info', name='info_url' ),
+    url( r'^usep/collections/$',  views.collections, name='collections_url' ),
+    url( r'^usep/collections/(?P<collection>[^/]+)/$',  views.collection, name='collection_url' ),
+    url( r'^usep/inscription/(?P<inscription_id>[^/]+)/$', views.display_inscription, name='inscription_url' ),
 
-    url( r'^usep/collections/$',  'usep_app.views.collections', name='collections_url' ),
-    url( r'^usep/collections/(?P<collection>[^/]+)/$',  'usep_app.views.collection', name='collection_url' ),
-    url( r'^usep/inscription/(?P<inscription_id>[^/]+)/$', 'usep_app.views.display_inscription', name='inscription_url' ),
+    url( r'^usep/publications/$',  views.publications, name='publications_url' ),
+    url( r'^usep/publication/(?P<publication>[^/]+)/$', views.pub_children, name='publication_url' ),
 
-    url( r'^usep/publications/$',  'usep_app.views.publications', name='publications_url' ),
-    url( r'^usep/publication/(?P<publication>[^/]+)/$', 'usep_app.views.pub_children', name='publication_url' ),
+    url( r'^usep/texts/$',  views.texts, name='texts_url' ),
+    url( r'^usep/links/$',  views.links, name='links_url' ),
+    url( r'^usep/about/$',  views.about, name='about_url' ),
+    url( r'^usep/contact/$',  views.contact, name='contact_url' ),
 
-    url( r'^usep/texts/$',  'usep_app.views.texts', name='texts_url' ),
-    url( r'^usep/links/$',  'usep_app.views.links', name='links_url' ),
-    url( r'^usep/about/$',  'usep_app.views.about', name='about_url' ),
-    url( r'^usep/contact/$',  'usep_app.views.contact', name='contact_url' ),
-
-    url( r'^usep/search/$', 'usep_app.search.search_form', name='search_url'),
-    url( r'^usep/search/results/$', 'usep_app.search.results', name='search_results_url'),
-
-    url( r'^usep/error_check/$', 'usep_app.views.error_check', name='error_check_url' ),  # only generates error if DEBUG == True
+    url( r'^usep/search/$', search.search_form, name='search_url'),
+    url( r'^usep/search/results/$', search.results, name='search_results_url'),
 
     url( r'^usep/$',  RedirectView.as_view(pattern_name='collections_url') ),
 
-    )
+    ## support urls...
+
+    url( r'^usep/info/$',  views.info, name='info_url' ),  ## TODO- retire after 2021-May-01
+    url( r'^usep/version/$',  views.info, name='info_url' ),
+    url( r'^usep/error_check/$', views.error_check, name='error_check_url' ),  # only generates error if DEBUG == True
+
+    ## other...
+
+    url( r'^usep/admin/links/$',  views.admin_links, name='admn_links_url' ),
+    url( r'^usep/admin/', include(admin.site.urls) ),
+
+    ]
